4971d2c84c84bf8164836ee6ff3f6299cee31eb9
Kevin Wasike Wakhisi <kevin@treeolive.tech>
2026-04-03 18:57:21 +0300

    fix(server): set DJANGO_SETTINGS_MODULE in server.py and remove vercel domain from allowed hosts

--- app/api/server.py
+++ app/api/server.py
@@ -1,3 +1,8 @@
+from os import environ
+
+from tawala import Package
 from tawala.api.backends import server_application
+
+environ.setdefault("DJANGO_SETTINGS_MODULE", Package.SETTINGS_MODULE)
 
 app = server_application
